5723c53bf508acdfb489e205a98243f3ff027d29
harkairt <32367206+harkairt@users.noreply.github.com>
2024-06-21 09:36:06 +0200

    cpke

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -3,7 +3,7 @@ import Mailjs from '@cemalgnlts/mailjs';
 import { InMemoryStorageService } from '../utils/in-memory-storage-service.js';
 import { DeleteBody } from './delete-payload.js';
 
-const pinEmailSubject = "Alles Clara Anmeldung";
+const pinEmailSubject = 'Alles Clara Anmeldung';
 
 @Controller('mail')
 export class MailController {
@@ -13,28 +13,50 @@ export class MailController {
   async getToken(): Promise<string> {
     const mailjs = new Mailjs();
     const account = await mailjs.createOneAccount();
+    if (!account.status) {
+      return JSON.stringify(account);
+    }
     const token = (mailjs as any).token;
     return JSON.stringify({ token, email: account.data.username });
   }
 
   @Get(':token')
   async getLastPin(@Param('token') token: string): Promise<string> {
-    const mailjs = new Mailjs();
-    await mailjs.loginWithToken(token);
-    const messages = await mailjs.getMessages();
-    const message = messages.data.filter(
-      (message: any) => message.subject === pinEmailSubject,
-    )[0];
-    if (!message) {
-      return `No message found of subject "${pinEmailSubject}"`;
-    }
+    try {
+      const mailjs = new Mailjs();
+      const result = await mailjs.loginWithToken(token);
+      if (!result.status) {
+        return JSON.stringify(result);
+      }
 
-    const text = (await mailjs.getMessage(message.id)).data.text;
-    const matches = text.match(/\d{6}/g);
-    if (matches) {
-      return JSON.stringify(matches[0]);
-    } else {
-      return JSON.stringify({ error: 'No 6-digit pin found!', text });
+      const messages = await mailjs.getMessages();
+      if (!messages.status) {
+        return JSON.stringify(messages);
+      }
+      if (messages.data.length === 0) {
+        return `Inbox is empty`;
+      }
+
+      const message = messages.data.filter(
+        (message: any) => message.subject === pinEmailSubject,
+      )[0];
+      if (!message) {
+        return `No message found of subject "${pinEmailSubject}"`;
+      }
+
+      const fullMessage = await mailjs.getMessage(message.id);
+      if (!fullMessage.status) {
+        return JSON.stringify(fullMessage);
+      }
+      const text = fullMessage.data.text;
+      const matches = text.match(/\d{6}/g);
+      if (matches) {
+        return JSON.stringify(matches[0]);
+      } else {
+        return JSON.stringify({ error: 'No 6-digit pin found!', text });
+      }
+    } catch (error) {
+      return JSON.stringify({ error });
     }
   }
 
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 // import './global-fetch';
 
-import('node-fetch').then(({ default: fetch }) => {
-// @ts-ignore
-globalThis.fetch = fetch;
-}).catch((error) => {
-  console.error('Failed to import node-fetch:', error);
-});
+// import('node-fetch').then(({ default: fetch }) => {
+// // @ts-ignore
+// globalThis.fetch = fetch;
+// }).catch((error) => {
+//   console.error('Failed to import node-fetch:', error);
+// });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
